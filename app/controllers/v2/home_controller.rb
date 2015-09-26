d6938564c82e78eb774a0d479862e8e2e3f9fcf6
Robin Boutros <niuage@gmail.com>
2015-09-25 21:55:19 -0400

    add site border and starting to add some stuff to the header

--- app/controllers/v2/home_controller.rb
+++ app/controllers/v2/home_controller.rb
@@ -1,7 +1,26 @@
 module V2
   class HomeController < V2::ApplicationController
     def index
+      project_class = Struct.new(:title, :description, :category, :image_url, :id)
 
+      projects_data = [
+        [
+          "HackerPond",
+          "Complete redesign of the old Devpost homepage, with a pond theme.",
+          "website",
+          "frog.png",
+          "frog"
+        ],
+        [
+          "Benefitsy",
+          "Find the good in food.",
+          "website",
+          "lemon.png",
+          "benefitsy"
+        ]
+      ]
+
+      @projects = projects_data.map { |d| project_class.new(*d) }
     end
   end
 end
