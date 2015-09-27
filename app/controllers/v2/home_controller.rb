640f1f2617dcb737af910791bcde18c80a55d643
Robin Boutros <niuage@gmail.com>
2015-09-27 14:22:47 -0400

    refined each project

--- app/controllers/v2/home_controller.rb
+++ app/controllers/v2/home_controller.rb
@@ -1,36 +1,44 @@
 module V2
   class HomeController < V2::ApplicationController
     def index
-      project_class = Struct.new(:title, :description, :category, :image_url, :id)
+      project_class = Struct.new(:title, :subtitle, :description, :category, :image_url, :id, :button_copy)
 
       projects_data = [
         [
           "HackerPond",
-          "Complete redesign of the old Devpost homepage, with a pond theme.",
+          "Developers, designers, product, UX... We're all hackers!",
+          "Complete redesign of the old Devpost homepage, with a pond theme. Just because.",
           "website",
           "frog.png",
-          "frog"
+          "frog",
+          "Jump in"
         ],
         [
           "Benefitsy",
           "Find the good in food.",
+          "Benefitsy's a simple way to learn about the health benefits of fruits, vegetables and herbs.",
           "website",
           "lemon.png",
-          "benefitsy"
+          "benefitsy",
+          "Take a bite"
         ],
         [
           "Spaceship",
-          "A better way to send your packages.",
+          "Ship anything, anywhere.",
+          "Chances are, a member is already headed that way, anyway.",
           "website",
           "spaceship.png",
-          "spaceship"
+          "spaceship",
+          "Launch"
         ],
         [
-          "Work at Devpost",
+          "Why I love Devpost",
+          "And why you will too.",
           "A one-page site showcasing the dev life at Devpost.",
           "website",
           "devpost_love.png",
-          "love-devpost"
+          "love-devpost",
+          "Come visit"
         ]
       ]
 
